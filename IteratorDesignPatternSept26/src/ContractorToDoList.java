724c0a8a1941ca6d0ed13a870ecbe17e6b36e69e
nylesgeiger <nylesgeiger@10.36.227.4>
2020-09-24 07:43:46 -0400

    Finished toDoIterator.

--- IteratorDesignPatternSept26/src/ContractorToDoList.java
+++ IteratorDesignPatternSept26/src/ContractorToDoList.java
@@ -2,10 +2,32 @@
 public class ContractorToDoList {
 
 	private String address;
-	private ToDo[] todos;
+	private ToDo[] todos = new ToDo[2];
 	private int count;
 	
 	public ContractorToDoList(String address) {
 		this.address = address;
+		
+	}
+	
+	public void addToDo() {
+		
+	}
+	
+	public ToDoIterator createIterator() {
+		return new ToDoIterator(todos);
+		
+	}
+	
+	public String getAddress() {
+		
+	}
+	
+	public double TotalCost() {
+		
+	}
+	
+	private ToDo[] growArray(ToDo[] todo[]) {
+		
 	}
 }
--- IteratorDesignPatternSept26/src/ToDoIterator.java
+++ IteratorDesignPatternSept26/src/ToDoIterator.java
@@ -1,4 +1,28 @@
+import java.util.Iterator;
 
-public class ToDoIterator {
+public class ToDoIterator implements Iterator{
 
+	private ToDo[] todos;
+	private int position;
+	
+	public ToDoIterator(ToDo[] todos) {
+		this.todos = todos;
+	}
+	
+	@Override
+	public ToDo next() {
+		if(!hasNext())return null;
+		ToDo todo = todos[position];
+		position += 1;
+		return todo;
+	}
+
+	@Override
+	public boolean hasNext() {
+		if(position >= todos.length || todos[position] == null) {
+			return false;
+		} else {
+		return true;
+		}
+	}
 }
