69ce51f1c4408a7536ec5c574cee1ced730b04d4
nylesgeiger <nylesgeiger@10.36.227.4>
2020-09-24 05:24:55 -0400

    Fixed class name and listed basic attributes and it's constructor within ContracorToDoList.

---
+++ IteratorDesignPatternSept26/src/ContractorToDoList.java
@@ -0,0 +1,11 @@
+
+public class ContractorToDoList {
+
+	private String address;
+	private ToDo[] todos;
+	private int count;
+	
+	public ContractorToDoList(String address) {
+		this.address = address;
+	}
+}
